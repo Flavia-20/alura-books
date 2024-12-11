66c918256c280c95c668e65e23642ba58073ce24
Flávia Monique <flaviamonique38@gmail.com>
2024-12-10 23:20:54 -0300

    Card reutilizável com props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
 import { Titulo }  from '../Titulo'
+import CardRecomenda from '../carsRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
@@ -29,6 +31,12 @@ function UltimosLancamentos() {
                     <img src={livro.src}/>
                 ) ) }
             </NovosLivrosContainer>
+            <CardRecomenda
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google"
+                img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
