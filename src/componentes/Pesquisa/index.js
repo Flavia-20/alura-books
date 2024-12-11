bbc267127b57b5c819c300758915c07d32a4b417
Flávia Monique <flaviamonique38@gmail.com>
2024-12-10 22:09:07 -0300

    Atualizando estados com onBlur

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from 'styled-components';
 import { useState } from 'react';
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -25,15 +26,21 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
-    const [ textoDigitado, setTextoDigitado ] = useState('');
+    const [ livrosPesquisados, setlivrosPesquisados ] = useState();
     return(
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro favorito na nossa estante</Subtitulo>
             <Input
-            placeholder="Encontre sua próxima leitura"
-            onBlur={evento => setTextoDigitado(evento.target.value)}/>
-        <p>{textoDigitado}</p>
+                placeholder="Encontre sua próxima leitura"
+                onBlur={evento =>{
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
+                    setlivrosPesquisados(resultadoPesquisa)
+                }}
+            />
+        
+        <p>{livrosPesquisados}</p>
         </PesquisaContainer>
     )
 }
