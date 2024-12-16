beca954365bc1f68cdb60e3c3681e08a4dcb9673
Flávia Monique <flaviamonique38@gmail.com>
2024-12-16 10:55:06 -0300

    estilo da página

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,4 +1,4 @@
-
+import livroImg from '../imagens/livro.png'
 import { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import { getFavoritos } from '../servicos/favoritos';
@@ -6,17 +6,25 @@ import { getFavoritos } from '../servicos/favoritos';
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
-
+    background-image: linear-gradient(90deg,#002F52 35%,#326589 165%);
 `
+const ResultadoContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
 const Resultado = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-bottom: 20px;
+    margin: 20px 0;
     cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
     p {
         width: 200px;
+        color: #FFF;
     }
     img {
         width: 100px;
@@ -24,6 +32,14 @@ const Resultado = styled.div`
     &:hover {
         border: 1px solid white;
     }
+`
+
+const Titulo = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
 `
 
 function Favoritos() {
@@ -40,12 +56,18 @@ function Favoritos() {
 
     return (
         <AppContainer>
-            { favoritos.map( favorito => (
+            <div>
+            <Titulo>Aqui estão seus livros favoritos:</Titulo>
+            <ResultadoContainer> 
+            { favoritos.length !== 0 ? favoritos.map( favorito => (
                 <Resultado>
-                    <img src={favorito.src}/>
+                   <img src={livroImg}/>
                     <p>{favorito.nome}</p>
                 </Resultado>
-            )) }
+            )) : null
+             }
+            </ResultadoContainer>
+            </div>
         </AppContainer>
   );
 }
